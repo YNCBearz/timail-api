7b41c48cabfc8b66a22d1a2cbc779091bf1a5164
YNCBearz <bear19930414@gmail.com>
2021-07-06 21:06:58 +0800

    feat: SignUpServiceTest (version1)

---
+++ tests/Unit/SignUpServiceTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Repositories\UserRepository;
+use App\Services\Portal\SignUpService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class SignUpServiceTest extends TestCase
+{
+    /**
+     * @var SignUpService $sut
+     */
+    protected SignUpService $sut;
+
+    /**
+     * @test
+     */
+    public function GivenRequest_WhenCreateNewAccount_ThenHashPassword()
+    {
+        //Arrange
+        $stubRequest = $this->createStub(Request::class);
+        $stubRequest->password = 123;
+
+        $stubUserRepository = $this->createStub(UserRepository::class);
+        $this->instance(UserRepository::class, $stubUserRepository);
+
+        $this->sut = $this->app->make(SignUpService::class);
+
+        //Assert
+        Hash::shouldReceive('make')->once()->with(123);
+
+        //Act
+        $this->sut->createNewAccount($stubRequest);
+    }
+
+}
